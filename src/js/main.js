02fdaacd9268a004bdcabc90b9955572c61ca291
plamen1982 <plamen.hristov@ymail.com>
2016-02-22 12:52:02 -0500

    Styling the responsive part

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-//
+$('button span').parent().click(function(){
+    if($('button span').hasClass('glyphicon-chevron-down')){
+        $('button').html('<span class="glyphicon glyphicon-chevron-up"></span>');
+    }
+    else
+    {
+        $('button').html('<span class="glyphicon glyphicon-chevron-down"></span>');
+    }
+});
